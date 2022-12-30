a1e446a7b184355e46e6b5c3e2ab592b4ae09889
vaniusrb <vanius@gmail.com>
2022-12-30 19:00:27 -0300

    impl axum's IntoResponse for Error Fixes #5

--- src/app/error.rs
+++ src/app/error.rs
@@ -1,3 +1,7 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use thiserror::Error as ThisError;
 
 #[derive(ThisError, Debug)]
@@ -6,4 +10,14 @@ pub enum Error {
     Database(#[from] sqlx::error::Error),
     #[error("validation error: `{0}`")]
     Validation(String),
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        match self {
+            Error::Database(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+            Error::Validation(e) => (StatusCode::BAD_REQUEST, e),
+        }
+        .into_response()
+    }
 }
--- src/controller/product.rs
+++ src/controller/product.rs
@@ -1,3 +1,4 @@
+use crate::app::error::Error;
 use crate::app::service::Service;
 use crate::{
     app::app_state::AppState,
@@ -6,7 +7,6 @@ use crate::{
 };
 use axum::{
     extract::{Path, Query, State},
-    http::StatusCode,
     response::IntoResponse,
     Json,
 };
@@ -15,62 +15,42 @@ use uuid::Uuid;
 pub async fn products_index(
     State(app_state): State<AppState>,
     pagination: Option<Query<Pagination>>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
+) -> Result<impl IntoResponse, Error> {
     let Pagination { limit, offset } = pagination.unwrap_or_default().0;
     let limit = limit.unwrap_or(Limit(10));
     let offset = offset.unwrap_or_default();
-    let list = app_state
-        .product_service()
-        .list(limit, offset)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let list = app_state.product_service().list(limit, offset).await?;
     Ok(Json(list))
 }
 
 pub async fn products_create(
     State(app_state): State<AppState>,
     Json(entity): Json<Product>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .product_service()
-        .create(entity)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.product_service().create(entity).await?;
     Ok(Json(entity))
 }
 
 pub async fn products_read(
     Path(id): Path<Uuid>,
     State(app_state): State<AppState>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .product_service()
-        .read(id)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.product_service().read(id).await?;
     Ok(Json(entity))
 }
 
 pub async fn products_update(
     State(app_state): State<AppState>,
     Json(entity): Json<Product>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .product_service()
-        .update(entity)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.product_service().update(entity).await?;
     Ok(Json(entity))
 }
 
 pub async fn products_delete(
     Path(id): Path<Uuid>,
     State(app_state): State<AppState>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .product_service()
-        .delete(id)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.product_service().delete(id).await?;
     Ok(Json(entity))
 }
