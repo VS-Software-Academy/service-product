98840f91ea602b8e8f637dc1abb9470d0ad884e1
vanius.rb <vanius@gmail.com>
2022-12-31 15:58:30 -0300

    Implemented IntoResponse for app Error

--- src/controller/category.rs
+++ src/controller/category.rs
@@ -1,3 +1,4 @@
+use crate::app::error::Error;
 use crate::app::service::Service;
 use crate::{
     app::app_state::AppState,
@@ -6,7 +7,6 @@ use crate::{
 };
 use axum::{
     extract::{Path, Query, State},
-    http::StatusCode,
     response::IntoResponse,
     Json,
 };
@@ -15,62 +15,42 @@ use uuid::Uuid;
 pub async fn categories_index(
     State(app_state): State<AppState>,
     pagination: Option<Query<Pagination>>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
+) -> Result<impl IntoResponse, Error> {
     let Pagination { limit, offset } = pagination.unwrap_or_default().0;
     let limit = limit.unwrap_or(Limit(10));
     let offset = offset.unwrap_or_default();
-    let list = app_state
-        .category_service()
-        .list(limit, offset)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let list = app_state.category_service().list(limit, offset).await?;
     Ok(Json(list))
 }
 
 pub async fn categories_create(
     State(app_state): State<AppState>,
     Json(entity): Json<Category>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .category_service()
-        .create(entity)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.category_service().create(entity).await?;
     Ok(Json(entity))
 }
 
 pub async fn categories_read(
     Path(id): Path<Uuid>,
     State(app_state): State<AppState>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .category_service()
-        .read(id)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.category_service().read(id).await?;
     Ok(Json(entity))
 }
 
 pub async fn categories_update(
     State(app_state): State<AppState>,
     Json(entity): Json<Category>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .category_service()
-        .update(entity)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.category_service().update(entity).await?;
     Ok(Json(entity))
 }
 
 pub async fn categories_delete(
     Path(id): Path<Uuid>,
     State(app_state): State<AppState>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let entity = app_state
-        .category_service()
-        .delete(id)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+) -> Result<impl IntoResponse, Error> {
+    let entity = app_state.category_service().delete(id).await?;
     Ok(Json(entity))
 }
